04b026d3ed76c606f3eeef569828f69b8e4b54f5
5hivani0 <149093767+5hivani0@users.noreply.github.com>
2023-11-04 10:05:07 +0000

    Create milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,6 @@
+#list of 5 favourite fruits
+favourite_fruits = ["apple", "grapes", "orange", "strawberry", "pineapple"]
+#assigned favourite fruits to variable called word_list
+word_list = favourite_fruits
+#print list of favourite fruit using assigned variable name word_list
+print(word_list)

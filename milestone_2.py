dc03769e99906b4744567d8cd5eb880408530e3a
5hivani0 <149093767+5hivani0@users.noreply.github.com>
2023-11-04 10:12:08 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,14 @@
+import random
+
 #list of 5 favourite fruits
 favourite_fruits = ["apple", "grapes", "orange", "strawberry", "pineapple"]
 #assigned favourite fruits to variable called word_list
 word_list = favourite_fruits
 #print list of favourite fruit using assigned variable name word_list
 print(word_list)
+
+# assign random fruit from word_list to the variable word
+word = random.choice(word_list)
+
+#print the randomly selected fruit
+print(word)

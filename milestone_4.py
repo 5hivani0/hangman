0128a8e6306c18d7bfea7c1a2f5c91c029996e40
Shivani Bhakta <shivanibhakta2000@gmail.com>
2023-11-05 12:41:04 +0000

    edit

--- milestone_4.py
+++ milestone_4.py
@@ -52,7 +52,7 @@ class Hangman():
 #testing the code
 if __name__ == "__main__":
   #list of 5 favourite fruits
-  favourite_fruits = ["apple", "grapes", "orange", "strawberry", "pineapple"]
+  favourite_fruits = ["lemon", "grapes", "orange", "peach", "apricot"]
   #assigned favourite fruits to variable called word_list
   word_list = favourite_fruits
   #print list of favourite fruit using assigned variable name word_list
